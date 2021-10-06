54fd7d73ef9b807c26c11e6f81a26e35727f71e3
ian02326 <90750504+ian02326@users.noreply.github.com>
2021-10-06 14:15:18 +0800

    20211006

--- Elevator_V1.js
+++ Elevator_V1.js
@@ -8,20 +8,36 @@ var targetFloor;
 while (true) {//無窮迴圈
 
     //輸入欲達樓層
-    targetFloor = readline.question('目前在' + currentFloor + '樓層')
+    targetFloor = readline.question('目前在' + currentFloor + '樓層,要去哪?')
     //判斷樓層是否合理
     targetFloor = parseInt(targetFloor);     //轉成整數
-    if (isNaN(targetFloor) || targetFloor < bottom || targetFloor > 10) {
-        console.log("請輸入介於" + bottom + " ~ " + TOP + "間的樓層")
+    if (isNaN(targetFloor) || targetFloor < bottom || targetFloor > top) {
+        console.log("請輸入介於" + bottom + " ~ " + top + "間的樓層")
         continue;
     }
-    if(targetFloor==currentFloor){
+    if (targetFloor == currentFloor) {
         console.log("離開樓層");
         break
-    }else{
+    } else {
         //移動電梯抵達樓層
         //currentFloor 移到 targetFloor
-        console.log("to be continue...")
+        if (targetFloor < currentFloor) {
+            console.log("電梯往下...");
+            while (targetFloor < currentFloor) {
+                currentFloor = currentFloor - 1
+                //currentFloor -= 1;
+                //currentFloor--;
+                console.log("電梯在" + currentFloor + "樓")
+            }
+        } else {              //上去
+            console.log("電梯往上...");
+            while (targetFloor > currentFloor) {
+                currentFloor = currentFloor + 1;
+                // currentFloor += 1;
+                // currentFloor++;
+                console.log("電梯在" + currentFloor + "樓");
+            }
+
+        }
     }
-   
 }
